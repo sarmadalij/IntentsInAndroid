df754d2b6f5ac52b2b93b19234f8809ed15ff5c2
Sarmad Ali Jalbani <developer.sarmadali@gmail.com>
2023-08-11 18:03:10 +0500

    Implicit Intent is also completed

--- app/src/main/java/com/sarmadali/intentsinandroid/MainActivity.java
+++ app/src/main/java/com/sarmadali/intentsinandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.sarmadali.intentsinandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +10,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView TextExplt;
+    TextView TextExplt, TextImplicit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //for explicit intent
         TextExplt = findViewById(R.id.explicitText);
 
         TextExplt.setOnClickListener(v -> {
@@ -26,6 +28,20 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent); // start of the intent || start of the next activity
 
 //                finish(); // to finish the intent and not to come back to same activity
+        });
+
+        //for implicit intent
+
+        TextImplicit = findViewById(R.id.implicitText);
+
+        TextImplicit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShareActivity.class);
+                startActivity(intent);
+
+//                finish();
+            }
         });
     }
 }
--- app/src/main/java/com/sarmadali/intentsinandroid/ShareActivity.java
+++ app/src/main/java/com/sarmadali/intentsinandroid/ShareActivity.java
@@ -2,13 +2,42 @@ package com.sarmadali.intentsinandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ShareActivity extends AppCompatActivity {
+
+    TextView quote;
+    Button share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        share = findViewById(R.id.sharebtn);
+        quote = findViewById(R.id.paragraph);
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String qt = quote.getText().toString(); //text into string
+
+                Intent sendTxt = new Intent(); //using intent to share text
+                sendTxt.setAction(Intent.ACTION_SEND); // selecting action of send
+                sendTxt.setType("text/plain"); // data type
+                sendTxt.putExtra(Intent.EXTRA_TEXT, qt); // putting extra
+
+                startActivity(sendTxt);
+
+
+
+
+            }
+        });
     }
 }
